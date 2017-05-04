788b17378cb83f2aa99a0ac67a54162601c299e6
Shawn Lewis <shlewis@gmail.com>
2017-05-04 08:40:00 -0700

    Take token out of submit.sh
    Also regenerated my crowdAI token so the token in git history is no
    longer valid.

--- submit.sh
+++ submit.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-KERAS_BACKEND=theano python submit.py ~/crowdai/OsimGait.h5 --token=6f5c48675ac83c834fae333443983039
+KERAS_BACKEND=theano python submit.py ~/crowdai/OsimGait.h5 --token=$1
